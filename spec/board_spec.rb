53c0d3ca4d3261a7c237e9e475f641cc4eebd068
Colin Suckow <csuckow99@gmail.com>
2020-06-09 09:39:38 -0700

    Add some fakes to board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,21 @@
-require_relative '../lib/board'
+require_relative "../lib/board"
 
-describe 'A Tic Tac Toe board' do
+describe "A Tic Tac Toe board" do
+  let(:full_board) {
+    {
+      top: { left: :fake, middle: :fake, right: :fake },
+      middle: { left: :fake, middle: :fake, right: :fake },
+      bottom: { left: :fake, middle: :fake, right: :fake },
+    }
+  }
+
+  let(:midgame_board) {
+    {
+      top: { left: :o, middle: nil, right: nil },
+      middle: { left: nil, middle: :x, right: nil },
+      bottom: { left: nil, middle: nil, right: nil },
+    }
+  }
 
   it "exists" do
     Board.new
@@ -15,5 +30,4 @@ describe 'A Tic Tac Toe board' do
   it "is initially empty" do
     expect(Board.new).to be_empty
   end
-
 end
