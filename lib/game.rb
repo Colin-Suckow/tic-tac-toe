3dbdf0c90b65da140c250dd4bd5400a24718579f
Colin Suckow <csuckow99@gmail.com>
2020-06-09 09:10:30 -0700

    Fix indentation

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,11 @@
-require_relative 'board'
+require_relative "board"
 
 class Game
-    def initialize(board = nil)
-        @board = board
-    end
+  def initialize(board = nil)
+    @board = board
+  end
 
-    def board
-        @board
-    end
+  def board
+    @board
+  end
 end
