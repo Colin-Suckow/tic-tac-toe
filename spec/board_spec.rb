51d0adbac69ec45e589013ed8d7ccb5ce9b13189
Colin Suckow <csuckow99@gmail.com>
2020-06-09 09:37:08 -0700

    Add initialization tests to board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,8 +2,18 @@ require_relative '../lib/board'
 
 describe 'A Tic Tac Toe board' do
 
-  it 'needs covered with tests' do
-    fail 'See spec/board_spec.rb'
+  it "exists" do
+    Board.new
+  end
+
+  it "can be created with existing locations" do
+    fake_locations = "Fake locations"
+    board = Board.new(fake_locations)
+    expect(board.locations).to eq(fake_locations)
+  end
+
+  it "is initially empty" do
+    expect(Board.new).to be_empty
   end
 
 end
